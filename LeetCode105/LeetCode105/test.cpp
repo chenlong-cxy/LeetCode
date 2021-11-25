f4710086d0ef1442f91835f5128629c44c5a36f0
chenlong-xcy <chenlonghyl@163.com>
2021-11-25 14:14:22 +0800

    根据前序和中序构建二叉树

--- LeetCode105/LeetCode105/test.cpp
+++ LeetCode105/LeetCode105/test.cpp
@@ -14,21 +14,25 @@ class Solution {
 public:
 	TreeNode* _buildTree(vector<int>& preorder, int& pi, vector<int>& inorder, int inStart, int inEnd)
 	{
-		if (inStart > inEnd)
+		if (inStart > inEnd) //中序序列不存在，该子树是空树
 			return nullptr;
-		TreeNode* root = new TreeNode(preorder[pi]);
+		//1、构建该子树的根结点
+		TreeNode* root = new TreeNode(preorder[pi]); 
 		pi++;
+		//2、将该子树的中序遍历序列划分为其左子树和右子树的中序遍历序列
 		int rooti = inStart;
 		while (inorder[rooti] != root->val)
 		{
 			rooti++;
 		}
+		//3、递归构建该子树的左右子树
 		root->left = _buildTree(preorder, pi, inorder, inStart, rooti - 1);
 		root->right = _buildTree(preorder, pi, inorder, rooti + 1, inEnd);
+		//4、返回所构建子树的根
 		return root;
 	}
 	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-		int i = 0;
+		int i = 0; //标识当前构建子树的根结点在preorder当中的下标
 		return _buildTree(preorder, i, inorder, 0, inorder.size() - 1);
 	}
 };
