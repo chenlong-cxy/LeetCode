27b04eaab918fb46c1be9908d6d76c80e242c064
chenlong-xcy <chenlonghyl@163.com>
2022-04-01 21:08:05 +0800

    用栈操作构建数组

--- LeetCode1441/LeetCode1441/test.cpp
+++ LeetCode1441/LeetCode1441/test.cpp
@@ -2,28 +2,29 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 class Solution {
 public:
 	vector<string> buildArray(vector<int>& target, int n) {
 		vector<string> ret;
 		size_t index = 0;
-		for (int i = 1; i <= n; i++)
+		int data = 1;
+		while (index < target.size())
 		{
-			if (target[index] == i)
+			//1、依次将list当中的数字data进行Push操作
+			ret.push_back("Push");
+			//2、data的值与当前遍历到的target当中的数字不匹配
+			if (data != target[index])
 			{
-				ret.push_back("Push");
-				index++;
-				if (index == target.size())
-					break;
+				ret.push_back("Pop"); //需要进行Pop操作
 			}
+			//3、data的值与当前遍历到的target当中的数字匹配
 			else
 			{
-				ret.push_back("Push");
-				ret.push_back("Pop");
+				index++; //继续遍历target当中的下一个数字进行构建
 			}
+			data++; //下一个data
 		}
-		return ret;
+		return ret; //返回操作序列
 	}
 };
 int main()
